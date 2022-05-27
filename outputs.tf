74b967077fccb2dcb97ceafa9bed18f27c1bea62
Everaldo Canuto <everaldo.canuto@gmail.com>
2022-05-27 02:33:53 +0000

    Output compartment_id

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "display_name" {
   value       = oci_core_vcn.vcn.display_name
 }
 
+output "compartment_id" {
+  description = "The OCID of the compartment to contain the VCN"
+  value       = oci_core_vcn.vcn.compartment_id
+}
+
 output "default_security_list_id" {
   description = "Default security list id for VCN"
   value       = oci_core_vcn.vcn.default_security_list_id
